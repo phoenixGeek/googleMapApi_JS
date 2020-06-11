ac8e91a45bba89362d498957a78246a9653c502c
ikram <devninjasuper0228@gmail.com>
2020-06-11 04:58:00 +0200

    features added

--- script.js
+++ script.js
@@ -7,7 +7,97 @@ var timer;
 
 function initMap() {
 
-    initialize();
+    // initialize();
+
+
+    //  additional practices using google map api
+
+
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //     center: { lat: -33.866, lng: 151.196 },
+    //     zoom: 15
+    // });
+
+    // var request = {
+    //     placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4',
+    // };
+
+    // var infowindow = new google.maps.InfoWindow();
+    // var service = new google.maps.places.PlacesService(map);
+
+    // service.getDetails(request, function (place, status) {
+    //     console.log(place)
+    //     if (status === google.maps.places.PlacesServiceStatus.OK) {
+    //         var marker = new google.maps.Marker({
+    //             map: map,
+    //             position: place.geometry.location
+    //         });
+    //         google.maps.event.addListener(marker, 'click', function () {
+    //             infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+    //                 'Place ID: ' + place.place_id + '<br>' +
+    //                 place.formatted_address + '</div>');
+    //             infowindow.open(map, this);
+    //         });
+    //     }
+    // });
+
+
+
+    // This example creates circles on the map, representing populations in North
+    // America.
+
+    // First, create an object containing LatLng and population for each city.
+    var citymap = {
+        chicago: {
+            center: { lat: 41.878, lng: -87.629 },
+            population: 2714856
+        },
+        newyork: {
+            center: { lat: 40.714, lng: -74.005 },
+            population: 8405837
+        },
+        losangeles: {
+            center: { lat: 34.052, lng: -118.243 },
+            population: 3857799
+        },
+        vancouver: {
+            center: { lat: 49.25, lng: -123.1 },
+            population: 603502
+        }
+    };
+
+    // Create the map.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: { lat: 37.090, lng: -95.712 },
+        mapTypeId: 'terrain'
+    });
+
+    // Construct the circle for each value in citymap.
+    // Note: We scale the area of the circle based on the population.
+    for (var city in citymap) {
+        console.log("here", city)
+        // Add the circle for this city to the map.
+        var cityCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: citymap[city].center,
+            radius: Math.sqrt(citymap[city].population) * 100
+        });
+    }
+
+
+
+
+
+
+
+
+
 }
 
 function initialize() {
